ded83b05ebe9fb0263497b10efd952599d29c477
AtyetiSuraj12398 <suraj.deosarkar@atyeti.com>
2025-06-19 18:53:40 +0530

    Multimodule Maven Project

---
+++ MultimoduleMavenProject/module2/src/main/java/Calculate.java
@@ -0,0 +1,11 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + Math.add(a, b));
+        System.out.println("Subtraction: " + Math.subtract(a, b));
+        System.out.println("Multiplication: " + Math.multiply(a, b));
+        System.out.println("Division: " + Math.divide(a, b));
+    }
+}
